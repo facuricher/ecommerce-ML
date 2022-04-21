c4476bcfa375cfe066d8d9470c60185f596c27d8
facuricher <facu.richer@gmail.com>
2022-04-21 00:42:40 -0300

    agregando componente Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React, {useState}from 'react'
+import { IconLogo,
+        IconLogoMobile,
+        Menu, 
+        MenuItem,
+        MenuItemLink, 
+        NavbarContainer, 
+        NavbarWrapper } 
+        from './styled';
+import { FaReact } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+     const [click, setClick] = useState(false)
+
+     const changeClick = ()=>{
+         setClick(!click)
+     }
+
+    return (
+        <>
+            <NavbarContainer>
+                <NavbarWrapper>
+                    <IconLogo>
+                    <FaReact size={'2em'}/>
+                        blogeate
+                    </IconLogo>
+
+                    <IconLogoMobile onClick= {()=> changeClick()}>
+                        {
+                            click ? <FaTimes/> : <FaBars/>
+                        }
+                    </IconLogoMobile>
+
+                    <Menu click={click}>
+                        <MenuItem onClick= {()=> changeClick()}>
+                            <MenuItemLink>HOME</MenuItemLink>
+                        </MenuItem>
+                        <MenuItem onClick= {()=> changeClick()}>
+                            <MenuItemLink>CHECK IN</MenuItemLink>
+                        </MenuItem>
+                        <MenuItem onClick= {()=> changeClick()}>
+                            <MenuItemLink>ABOUT US</MenuItemLink>
+                        </MenuItem>
+                        <MenuItem onClick= {()=> changeClick()}>
+                            <MenuItemLink>CONTACT</MenuItemLink>
+                        </MenuItem>
+                    </Menu>
+                </NavbarWrapper>
+            </NavbarContainer>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/Components/Navbar/styled.js
@@ -0,0 +1,81 @@
+import styled  from 'styled-components'
+
+export const NavbarContainer = styled.div `
+    width: 100%;
+    height: 80px;
+    position: sticky;
+    top: 0;
+    z-index: 99;
+    background-color:#072227;
+`
+
+export const NavbarWrapper = styled.div`
+    margin: auto;
+    width: 100%;
+    max-width: 1300px;
+    height: 100%;
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
+
+export const IconLogo = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: center;
+    font-size: 1.2rem;
+    color: #fafafa;
+    padding-left: 1rem;
+`
+
+export const Menu = styled.ul`
+    height: 100%;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+    @media screen and (max-width:960px) {
+        width:100%;
+        height:90vh;
+        position: absolute;
+        top: 80px;
+        left: ${({click}) => (click ? 0 : "-100%")};
+        flex-direction: column;
+        transition: 0.5s all ease-in;
+        background-color:#072227;
+    }
+`
+
+export const MenuItem =styled.li`
+    height:100%;
+    padding: 0.5rem 1.5rem;
+    display:flex;
+    align-items:center;
+    font-size:1.2rem;
+    font-weight: 400;
+    &:hover{
+        background-color:#35858B;
+        border-bottom:0.3rem solid #fafafa;
+        transition:0.4s ease-in;
+    }
+    @media screen and (max-width:960px){
+        width:100%;
+        height:100px;
+    }
+`
+
+export const MenuItemLink= styled.a`
+    text-decoration: none;
+    color: #fafafa;
+`
+
+export const IconLogoMobile = styled.div`
+    display: none;
+    @media screen and (max-width:960px) {
+        display: flex;
+        color:#fafafa;
+        font-size:  2rem;
+        padding-right:1rem;
+    }
+`
